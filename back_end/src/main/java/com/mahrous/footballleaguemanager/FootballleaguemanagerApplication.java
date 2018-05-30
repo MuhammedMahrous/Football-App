a26adebbbd48fd7813185f18ac084a997bb42ef9
MuhammedMahrous <muhamed.mahrous.muhamed.kamel@gmail.com>
2018-05-30 21:08:23 +0200

    Front end and back end init

---
+++ back_end/src/main/java/com/mahrous/footballleaguemanager/FootballleaguemanagerApplication.java
@@ -0,0 +1,12 @@
+package com.mahrous.footballleaguemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballleaguemanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballleaguemanagerApplication.class, args);
+    }
+}
